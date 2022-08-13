3e63fbb454e6c050215234c0be96e51eac47ce4a
MohA39 <105949391+MohA39@users.noreply.github.com>
2022-08-13 02:35:41 +0200

    Fixed project names, added more comments.

---
+++ ClientDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using I2PSharp;
+
+SAMSession session = new SAMSession(7656);
+
+// Connect to SAM
+await session.ConnectAsync();
+
+// Generate keys
+(string PrivateKey, string PublicKey) Keys = await session.GenerateDestinationAsync();
+
+// Create a primary session using the private key & print the public key
+await session.CreateSessionAsync(Keys.PrivateKey);
+Console.WriteLine("Public key: " + Keys.PublicKey);
+
+// Create STREAM subsession
+SAMSubsession subsession = await session.CreateSTREAMSubsessionAsync();
+subsession.OnConnect += subsession_OnConnect;
+
+// Get server public key
+Console.Write("Server public key: ");
+string PublicKey = Console.ReadLine();
+
+// Connect to server
+PeerConnection cs = await subsession.Connect(PublicKey);
+cs.OnMessage += Cs_OnMessage;
+cs.OnDisconnect += Cs_OnDisconnect;
+
+void Cs_OnDisconnect(object sender, DisconnectEventArgs e)
+{
+    Console.WriteLine("Peer disconnected");
+}
+
+while (true)
+{
+    cs.SendString(Console.ReadLine());
+}
+
+void Cs_OnMessage(object sender, MessageEventArgs e)
+{
+    Console.WriteLine("Recieved: " + e.Message);
+}
+
+
+void subsession_OnConnect(object sender, ConnectionEventArgs e)
+{
+    Console.WriteLine("Subsession connected to by " + e.ConnectionType);
+}
--- ServerDemo/Program.cs
+++ ServerDemo/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using I2PSharp;
+using System.Threading;
+
+// Default SAM port: 7656
+SAMSession session = new SAMSession(7656);
+
+// List of all accepted peers.
+List<PeerConnection> connections = new List<PeerConnection>();
+
+// Connect to SAM
+await session.ConnectAsync();
+
+// Generate keys
+(string PrivateKey, string PublicKey) Keys = await session.GenerateDestinationAsync();
+
+// Create a primary session using the private key & print the public key
+await session.CreateSessionAsync(Keys.PrivateKey);
+Console.WriteLine("Public key: " + Keys.PublicKey);
+
+// Create STREAM subsession
+SAMSubsession subsession = await session.CreateSTREAMSubsessionAsync();
+subsession.OnConnect += subsession_OnConnect;
+
+new Thread(new ThreadStart(async () => 
+{
+    while (true)
+    {
+        // Wait for a connection
+        PeerConnection cs = await subsession.AcceptConnections();
+        cs.OnMessage += Cs_OnMessage;
+        cs.OnDisconnect += Cs_OnDisconnect;
+
+        connections.Add(cs);
+    }
+})).Start();
+
+
+
+while (true)
+{
+    string MessageToSend = Console.ReadLine();
+    foreach (PeerConnection cs in connections)
+    {
+        // Send the message
+        cs.SendString(MessageToSend);
+    }
+}
+
+void Cs_OnDisconnect(object sender, DisconnectEventArgs e)
+{
+    Console.WriteLine("Peer disconnected");
+}
+
+void Cs_OnMessage(object sender, MessageEventArgs e)
+{
+    Console.WriteLine("Recieved: " + e.Message);
+}
+
+
+void subsession_OnConnect(object sender, ConnectionEventArgs e)
+{
+    Console.WriteLine("Subsession connected by " + e.ConnectionType);
+}
